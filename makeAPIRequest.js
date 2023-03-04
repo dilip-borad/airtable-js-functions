f02b922a320a83f06d072a18198e942b281ce488
Dilip Borad <dilipborad@gmail.com>
2023-03-04 10:29:43 +0530

    Create makeAPIRequest.js
    Created new file for makeAPIRequest.

---
+++ makeAPIRequest.js
@@ -0,0 +1,21 @@
+/**
+ * Make simple common API reuest. 
+ * ==============================
+ * @param {Object} obj with The paramters { url: `${url}`, method: requestMethod, headers: {"header1Key": "header1Value", "header2Key": "header2Value"} body: {}}
+ * @return {Object} data response.
+ **/
+const makeAPIRequest = async function (obj) {
+    let options = {}, response;
+    options.method = obj.method;
+    if (typeof obj.body !== undefined) {
+        options.body = JSON.stringify(obj.body);
+    }
+    if (typeof obj.headers !== undefined) {
+        options.headers = obj.headers;
+    }
+    output.markdown(`Make request on \`${obj.url}\``);
+    //console.log(obj);    
+    response = await remoteFetchAsync(obj.url, options);
+    response = await response.json();
+    return response;
+}
