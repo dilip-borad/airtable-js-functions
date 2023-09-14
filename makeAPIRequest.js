49d9102e8a7a66395c2ea3ec1cc3172898139fda
Dilip Borad <dilipborad@gmail.com>
2023-09-14 11:38:49 +0530

    Update makeAPIRequest.js
    Just make code shorter.

--- makeAPIRequest.js
+++ makeAPIRequest.js
@@ -4,18 +4,14 @@
  * @param {Object} obj with The paramters { url: `${url}`, method: requestMethod, headers: {"header1Key": "header1Value", "header2Key": "header2Value"} body: {}}
  * @return {Object} data response.
  **/
-const makeAPIRequest = async function (obj) {
-    let options = {}, response;
-    options.method = obj.method;
-    if (typeof obj.body !== undefined) {
-        options.body = JSON.stringify(obj.body);
-    }
-    if (typeof obj.headers !== undefined) {
-        options.headers = obj.headers;
-    }
+const makeAPIRequest = async (obj) => {
+    const options = {
+        method: obj.method,
+        body: obj.body ? JSON.stringify(obj.body) : undefined,
+        headers: obj.headers || {},
+    };
+
     output.markdown(`Make request on \`${obj.url}\``);
-    //console.log(obj);    
-    response = await fetch(obj.url, options);
-    response = await response.json();
-    return response;
-}
+    const response = await fetch(obj.url, options);
+    return await response.json();
+};
